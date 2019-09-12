1099231de3ba87413cc87dd8ef194edcfa5ff528
Luke Poskitt <luke.poskitt@gmail.com>
2019-09-12 12:00:45 +1000

    Cast flash and memory attributes to Net::AnywhereUSB::Common::Value objects.

--- lib/Net/AnywhereUSB/System/Information.pm
+++ lib/Net/AnywhereUSB/System/Information.pm
@@ -3,6 +3,8 @@ package Net::AnywhereUSB::System::Information;
 use strict;
 use warnings;
 
+use Net::AnywhereUSB::Common::Value;
+
 our @ATTR = qw(
 	boot_version 
 	cpu_utilization 
@@ -25,10 +27,17 @@ our @ATTR = qw(
 { 
 no strict 'refs';
 
-	for my $attr ( @ATTR ) {
+	for my $attr ( grep !/(flash|memory)/, @ATTR ) {
 		*{ __PACKAGE__ . "::$attr" } = sub {
 			my $self = shift;
 			return $self->{ $attr }
+		}
+	}
+
+	for my $attr ( grep /(flash|memory)/, @ATTR ) {
+		*{ __PACKAGE__. "::$attr\_raw" } = sub {
+			my $self = shift;
+			return Net::AnywhereUSB::Common::Value->new( $self->{ $attr } )
 		}
 	}
 }
@@ -45,11 +54,30 @@ sub new {
 sub __init {
 	my ( $self, %args ) = @_;
 
-	for my $k ( keys %args ) {
-		$self->{ $k } = $args{ $k } 
+	for my $attr ( @ATTR ) {
+		$self->{ $attr } = $args{ $attr } 
 	}
 }
 
 1;
 
 __END__
+
+
+'used_flash_filesystem' => '21 KB',
+'post_version' => '1.09.9.39 (release 82001552_A generic_travis hwdiag 11/09/2009)',
+'hardware_strapping' => '0x0000',
+'total_flash_filesystem' => '553 KB',
+'total_memory' => '131072 KB',
+'cpu_utilization' => '1%',
+'used_memory' => '11463 KB',
+'product_id' => '0x0121',
+'model' => 'AnywhereUSB/14',
+'ethernet_mac_address' => '00:40:9D:44:F3:96',
+'product_vpd_version' => 'none',
+'up_time' => '2 days 3 hours 32 minutes 27 seconds',
+'boot_version' => '1.09.10.6 (release 82002082_B awusb14 bootloader 11/09/2009)',
+'firmware_version' => '1.97.22.4 (build 82002081_R1 awusb14 eos 1/22/2019 11:31:26a)',
+'free_memory' => '119609 KB',
+'free_flash_filesystem' => '532 KB'
+
