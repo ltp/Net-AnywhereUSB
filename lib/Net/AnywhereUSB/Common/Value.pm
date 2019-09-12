ed086d76838c93fa2eda50d1d34468e30443d6ec
Luke Poskitt <luke.poskitt@gmail.com>
2019-09-12 12:13:01 +1000

    New methods.

--- lib/Net/AnywhereUSB/Common/Value.pm
+++ lib/Net/AnywhereUSB/Common/Value.pm
@@ -6,24 +6,54 @@ use warnings;
 use overload '""' => '__as_string';
 
 sub new {
-	my ( $class, $value  ) = @_;
+	my ( $class, $arg  ) = @_;
 
-	( $value, $units ) = ( split / /, $value );
 	my $self = bless {} , $class;
-	$self->{ value } = $args{ value };
-	$self->{ units } = $args{ units };
+	$self->__init( $arg );
 
 	return $self
 }
 
+sub __init {
+	my ( $self, $arg ) = @_;
+
+	my ( $value, $units ) = ( split / /, $arg );
+	$self->{ value } = $value;
+	$self->{ units } = $units;
+}
+
+sub value {
+	return $_[0]->{ value }
+}
+
+sub units {
+	return $_[0]->{ units }
+}
+
 sub to_bits {
-	
+	my $self = shift;
+
+	if ( ( lc $self->{ units } ) eq 'b' ) {
+		return $self->{ value }
+	}
+	elsif ( ( lc $self->{ units } ) eq 'kb' ) {
+		return ( $self->{ value } * 1024 )
+	}
+	elsif ( ( lc $self->{ units } ) eq 'mb' ) {
+		return ( $self->{ value } * 1024 * 1024 )
+	}
+	elsif ( ( lc $self->{ units } ) eq 'gb' ) {
+		return ( $self->{ value } * 1024 * 1024 * 1024 )
+	}
+	else {
+		return $self->{ value }
+	}
 }
 
 sub __as_string {
 	my $self = shift;
 
-	return $self->{ value }
+	return $self->{ value }." ".$self->{ units }
 }
 
 1;
