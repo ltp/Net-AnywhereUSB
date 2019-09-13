7554e8bbed133eac1527407bc62e80d06dfbfd48
Luke Poskitt <luke.poskitt@gmail.com>
2019-09-13 13:40:07 +1000

    Added POD.

--- lib/Net/AnywhereUSB/System/Summary.pm
+++ lib/Net/AnywhereUSB/System/Summary.pm
@@ -44,3 +44,51 @@ sub __init {
 1;
 
 __END__
+
+=pod
+
+=name1 NAME
+
+Net::AnywhereUSB::System::Summary - Utility class for representing system
+summary data returned from Digi AnywhereUSB devices.
+
+=head1 SYNOPSIS
+
+This module is a utility class for representing information system summary
+information returned from Digi AnywhereUSB devices.
+
+Note that you should not instantiate objects of this class directly - rather
+objects of this class will returned when invoking the summary() method in the 
+L<Net::AnywhereUSB> module.
+
+=head1 METHODS
+
+=head2 contact ( )
+
+Returns the device contact.
+
+=head2 description ( )
+
+Returns the device description.
+
+=head2 device_id ( )
+
+returns the device ID.
+
+=head2 ethernet_ip_address ( )
+
+Returns the device IP address.
+
+=head2 ethernet_mac_address ( )
+
+Returns the device MAC address.
+
+=head2 location ( )
+
+Returns the device location.
+
+=head2 model ( )
+
+Returns the device model string.
+
+=cut
